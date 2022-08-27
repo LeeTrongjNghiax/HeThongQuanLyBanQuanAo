085d7017d8acce311f2f0734ab6d1933a97e07c9
toanguyen020102 <108285002+toanguyen020102@users.noreply.github.com>
2022-08-27 12:15:22 +0700

    1

--- program/demo/querys/main.sql
+++
@@ -1,4 +0,0 @@
-BACKUP DATABASE QuanLyNhanVien TO DISK = 'C:\Users\LeeTrongjNghiax\OneDrive - Industrial University of HoChiMinh City\year3\HK1\phatTrienUngDung\homeworks\HeThongQuanLyBanQuanAo\program\demo\querys\backups.bak'
-BACKUP DATABASE QuanLyNhanVien TO DISK = 'C:\backups\test.bak'
-
-RESTORE DATABASE QuanLyNhanVien FROM DISK = 'C:\backups\test.bak'
