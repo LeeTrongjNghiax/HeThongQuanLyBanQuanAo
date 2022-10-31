4d0d792a74bbfbb1cb32e6798ac0df31cb21e9fe
Nguyễn Đức Long <90436108+Long7032@users.noreply.github.com>
2022-10-31 18:32:15 +0700

    Update taoBang.sql

--- querys/taoBang.sql
+++ querys/taoBang.sql
@@ -1,18 +1,16 @@
-CREATE DATABASE QuanLyCuaHangBanQuanAo;
+create database QLBHTTrang
 
-USE QuanLyCuaHangBanQuanAo;
+USE QLBHTTrang
 
 CREATE TABLE NhanVien(
-	maNhanVien NVARCHAR(6) PRIMARY KEY,
+	maNhanVien VARCHAR(6) PRIMARY KEY,
 	tenNhanVien NVARCHAR(100) NOT NULL,
-	soDienThoai NVARCHAR(10) NOT NULL,
+	soDienThoai VARCHAR(10) NOT NULL,
 	gioiTinh BIT NOT NULL,
 	ngaySinh DATE NOT NULL,
-	gioiTinh BIT NOT NULL,
-	ngaySinh DATE NOT NULL,
-	email NVARCHAR(100) NOT NULL,
-	ngayVaoLam DATE NOT NULL,
-	chungMinhNhanDan NVARCHAR(12) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	ngayVaoLam DATE NOT NULL,	
+	chungMinhNhanDan VARCHAR(12) NOT NULL,
 	chucVu NVARCHAR(50) NOT NULL,
 	thanhpho NVARCHAR(100) NOT NULL,
 	quan NVARCHAR(100) NOT NULL,
@@ -20,87 +18,83 @@ CREATE TABLE NhanVien(
 	diaChi NVARCHAR(100) NOT NULL
 )
 
-
 CREATE TABLE KhachHang(
-	maKhachHang NVARCHAR(30) PRIMARY KEY,
+	maKhachHang VARCHAR(30) PRIMARY KEY,
 	tenKhachHang NVARCHAR(100) NOT NULL,
 	gioiTinh BIT NOT NULL,
-	soDienThoai NVARCHAR(10) NOT NULL,
+	soDienThoai VARCHAR(10) NOT NULL,
 	ngaySinh DATE NOT NULL,
-	email NVARCHAR(100) NOT NULL,
-	diemTichLuy INT NOT NULL
+	email VARCHAR(100),
+	diemTichLuy INT check (diemTichLuy >= 0)
 )
 
 CREATE TABLE TaiKhoan(
-	tenTaiKhoan NVARCHAR(30) PRIMARY KEY,
-	matKhau NVARCHAR(50) NOT NULL,
-	maNhanVien NVARCHAR(30) NOT NULL,
+	tenTaiKhoan VARCHAR(30) PRIMARY KEY,
+	matKhau VARCHAR(50) NOT NULL,
+	maNhanVien VARCHAR(6) NOT NULL,
+
 	FOREIGN KEY (maNhanVien) REFERENCES NhanVien(maNhanVien) on delete cascade
 )
 
 CREATE TABLE LoaiSanPham(
-	maLoaiSanPham NVARCHAR(30) PRIMARY KEY,
+	maLoaiSanPham VARCHAR(30) PRIMARY KEY,
 	tenLoaiSanPham NVARCHAR(50) NOT NULL
 )
 
 CREATE TABLE SanPham(
-	maSanPham NVARCHAR(30) PRIMARY KEY,
+	maSanPham VARCHAR(30) PRIMARY KEY,
 	tenSanPham NVARCHAR(100) NOT NULL,
 	chatLieu NVARCHAR(50) NOT NULL,
 	kieuDang NVARCHAR(50) NOT NULL,
-	phuHopGioiTinh NVARCHAR(50) NOT NULL,
+	phuHopGioiTinh BIT NOT NULL,
 	donViSanPham NVARCHAR(10) NOT NULL,
 	moTaSanPham NVARCHAR(1000) NOT NULL,
 	thuongHieu NVARCHAR(50) NOT NULL,
 	nuocThuongHieu NVARCHAR(50) NOT NULL,
 	tenNhaSanXuat NVARCHAR(50) NOT NULL,
-	soDienThoaiNhaSanXuat NVARCHAR(10) NOT NULL,
-	emailNhaSanXuat NVARCHAR(50) NOT NULL,
-	noiSanXuat NVARCHAR(50) NOT NULL,
+	soDienThoaiNhaSanXuat VARCHAR(10) NOT NULL,
+	emailNhaSanXuat VARCHAR(50) NOT NULL,
 	duongDan NVARCHAR(300) NOT NULL,
-	maLoaiSanPham NVARCHAR(30) NOT NULL,
-	maNhanVien NVARCHAR(30) NOT NULL,
+	maLoaiSanPham VARCHAR(30) NOT NULL,
+	maNhanVien VARCHAR(6) NOT NULL,
 
 	FOREIGN KEY (maLoaiSanPham) REFERENCES LoaiSanPham(maLoaiSanPham),
 	FOREIGN KEY (maNhanVien) REFERENCES NhanVien(maNhanVien)
 )
 
 CREATE TABLE ChiTietSanPham(
-	mauSac NVARCHAR(100) NOT NULL,
-	kichCo NVARCHAR(3) NOT NULL,
+	maSanPham VARCHAR(30) NOT NULL,
+	kichCo VARCHAR(5) NOT NULL,
+	mauSac VARCHAR(100) NOT NULL,
 	soLuong INT NOT NULL,
 	giaNhap FLOAT NOT NULL,
-	maSanPham NVARCHAR(30) NOT NULL,
 
-	FOREIGN KEY (maSanPham) REFERENCES SanPham(maSanPham),
+	FOREIGN KEY (maSanPham) REFERENCES SanPham(maSanPham) on delete cascade
 )
 
 CREATE TABLE HoaDon(
-	maHoaDon NVARCHAR(10) PRIMARY KEY, 
+	maHoaDon VARCHAR(10) PRIMARY KEY, 
 	thoiGianTaoHoaDon DATETIME NOT NULL,
+	maKhachHang VARCHAR(30) NOT NULL,
+	maNhanVien VARCHAR(6) NOT NULL,
 	caLamViec INT NOT NULL,
+	tongTien FLOAT NOT NULL,
+	tongGiam FLOAT NOT NULL,
+	tongPhaiTra FLOAT NOT NULL,
 	tienNhan FLOAT NOT NULL,
-	maKhachHang NVARCHAR(30) NOT NULL,
-	maNhanVien NVARCHAR(30) NOT NULL,
+	tienTra FLOAT NOT NULL,	
 
 	FOREIGN KEY (maKhachHang) REFERENCES KhachHang(maKhachHang),
 	FOREIGN KEY (maNhanVien) REFERENCES NhanVien(maNhanVien)
 )
 
 CREATE TABLE ChiTietHoaDon(
-	soLuong INT NOT NULL,
-	maSanPham NVARCHAR(30) NOT NULL,
-	maHoaDon NVARCHAR(10), 
+	maHoaDon VARCHAR(10), 
+	maSanPham VARCHAR(30) not null,
+	tenSanPham NVARCHAR(30) NOT NULL,	
+    soLuong INT NOT NULL,
+	giaBan FLOAT NOT NULL,
 
 	FOREIGN KEY (maSanPham) REFERENCES SanPham(maSanPham),
-	FOREIGN KEY (maHoaDon) REFERENCES HoaDon(maHoaDon)
+	FOREIGN KEY (maHoaDon) REFERENCES HoaDon(maHoaDon) on delete cascade,
 )
-
-DROP TABLE KhachHang
-DROP TABLE NhanVien
-DROP TABLE TaiKhoan
-DROP TABLE LoaiSanPham
-DROP TABLE SanPham
-DROP TABLE ChiTietSanPham
-DROP TABLE HoaDon
-DROP TABLE ChiTietHoaDon
